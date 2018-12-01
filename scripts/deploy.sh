de4d571e65a9d02cf26b860363f5a4699fff2c1f
rsilveira65 <rsilveiracc@gmail.com>
2018-12-01 16:49:16 -0200

    Incrementing version number to v

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+versionFile="version.txt"
 
 git checkout raspberry-deploy
 git add .
-git commit -m'Deploy'
-git push origin raspberry-deploy
+
+versionLabel=v$1
+
+sed -i.backup -E "s/\= v[0-9.]+/\= $versionLabel/" $versionFile $versionFile
+rm $versionFile.backup
+
+git commit -am "Incrementing version number to $versionLabel"
+
+git tag $versionLabel
+
+git push origin raspberry-deploy && git push --tags
